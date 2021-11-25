7a064bbf6a7c6954975ba219df83c9e190270cc4
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-11-25 15:09:03 +0000

    2nd solution

--- index.js
+++ index.js
@@ -25,4 +25,16 @@ var findMaxConsecutiveOnes = function(nums) {
     }
     return max;
 
+//solution 2
+    let largest = 0;
+    let current = 0;
+    for (let i = 0; i <nums.length; i++) {
+
+        nums[i] === 0 ? current = 0 : current++;
+
+        if (current > largest) largest = current;
+    };
+
+    return largest;
+
 }
